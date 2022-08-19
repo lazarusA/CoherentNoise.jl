0cf57a58a65d3c6262295ade72e0d3432ee0ca6d
Michael Fiano <mfiano@users.noreply.github.com>
2022-08-19 06:17:04 -0400

    Update versions.js

--- versions.js
+++ versions.js
@@ -1,8 +1,6 @@
 var DOC_VERSIONS = [
   "stable",
-  "v1.0",
-  "v0.1",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v1.0.1";
-var DOCUMENTER_STABLE = "stable";
+var DOCUMENTER_NEWEST = "dev";
+var DOCUMENTER_STABLE = "dev";
