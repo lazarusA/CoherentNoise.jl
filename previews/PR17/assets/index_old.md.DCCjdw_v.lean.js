fd036a9e2d6d5257d30bf26e5f5615690c767f12
Documenter.jl <documenter@juliadocs.github.io>
2024-10-08 18:00:55 +0000

    build based on 29db256

---
+++ previews/PR17/assets/index_old.md.DCCjdw_v.lean.js
@@ -0,0 +1,14 @@
+import{_ as r,c as t,a5 as i,o as s}from"./chunks/framework.DxZZ2Gkk.js";const o="/CoherentNoise.jl/previews/PR17/assets/splash.C1f59Qi2.png",g=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"index_old.md","filePath":"index_old.md","lastUpdated":null}'),l={name:"index_old.md"};function a(n,e,p,d,c,f){return s(),t("div",null,e[0]||(e[0]=[i('<p><img src="'+o+`" alt=""></p><ul><li><p><strong>Overview</strong></p><pre><code>                      - [What-is-coherent-noise?](overview#What-is-coherent-noise?)
+                          - [History](overview#History)
+                              - [What&#39;s-different-from-other-libraries?](overview#What&#39;s-different-from-other-libraries?)
+                                      - [Available-samplers](overview#Available-samplers)
+</code></pre></li><li><p><strong>Algorithms</strong></p></li><li><p><a href="./algorithms#Patterns">Patterns</a></p><ul><li><a href="./algorithms#Noise">Noise</a><ul><li><a href="./algorithms#Fractals">Fractals</a></li></ul></li></ul></li><li><p><strong>Getting Started</strong></p></li><li><p><strong>Tutorial</strong></p><pre><code>                                                              - [first_steps](tutorial#first_steps)
+                                                                  - [Visualizing-results](tutorial#Visualizing-results)
+                                                                      - [Modifiers](tutorial#Modifiers)
+                                                                          - [Pipelines](tutorial#Pipelines)
+                                                                              - [Fractals](tutorial#Fractals)
+                                                                                              - [Adding-color](tutorial#Adding-color)
+</code></pre></li><li><p><strong>Gallery</strong></p><pre><code>      - [user_creations](gallery#user_creations)
+
+                                                          - [API-Reference](reference#API-Reference)
+</code></pre></li></ul>`,2)]))}const h=r(l,[["render",a]]);export{g as __pageData,h as default};
